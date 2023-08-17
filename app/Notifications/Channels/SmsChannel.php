306256e640af1ce738de790ab25b7c043f129af8
Farshid Rezaei <f.rezaei@vandar.io>
2023-08-17 22:15:52 +0330

    add sms notification channel

---
+++ app/Notifications/Channels/SmsChannel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Notifications\Channels;
+
+use App\Facades\Sms;
+use App\Models\User;
+use App\Notifications\CardToCardIncreaseBalanceNotification;
+
+class SmsChannel
+{
+    /**
+     * @param User $notifiable
+     * @param CardToCardIncreaseBalanceNotification $notification
+     */
+    public function send(User $notifiable, CardToCardIncreaseBalanceNotification $notification): void
+    {
+        Sms::send($notifiable->routeNotificationForSms(), $notification->toSms());
+    }
+}
