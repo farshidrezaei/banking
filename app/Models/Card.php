73aff8aee24ed3ae92baff230cdd9bede6765063
Farshid Rezaei <f.rezaei@vandar.io>
2023-08-17 21:31:24 +0330

    add card data model

---
+++ app/Models/Card.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Card extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+
+    protected $guarded = ['id'];
+
+    protected $hidden = ['password'];
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+
+}
